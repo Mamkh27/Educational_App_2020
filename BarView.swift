b7d2295594c85c828af4df4c3f2bfab5b5498650
mampz <mkheer@ucdavis.edu>
2020-04-23 23:01:01 -0700

    Quick iOS Demo SwiftUI

--- BarView.swift
+++ BarView.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 struct BarView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            ZStack {
+                Color(red: 42/255, green:96/255, blue:48/255)
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 100)
+                Color(red: 174/255, green: 146/255, blue:73/255)
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 10)
+                    .offset(x: 0, y: 45)
+                        
+        }
+
+                     
+        }
+
     }
-}
 
 struct BarView_Previews: PreviewProvider {
     static var previews: some View {
